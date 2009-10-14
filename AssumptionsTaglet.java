d466657c464f129116562ebffd7b495ccf4de167
Terin Stock <terinjokes@gmail.com>
2009-10-14 01:17:10 -0500

    Initial commit of Terin's Taglets.

---
+++ AssumptionsTaglet.java
@@ -0,0 +1,147 @@
+import com.sun.tools.doclets.Taglet;
+import com.sun.javadoc.*;
+import java.util.Map;
+
+/**
+ * A Taglet representing @assumption. This tag can be used in any kind of
+ * {@link com.sun.javadoc.Doc}.  It is not an inline tag. The text is displayed
+ * in like @param or @returns.
+ *
+ * @author Terin Stock
+ * @version 10.13.09
+ * @license GNU LGPL
+ */
+
+public class AssumptionsTaglet implements Taglet {
+    
+    private static final String NAME = "assumption";
+    private static final String HEADER = "Assumptions:";
+    
+    /**
+     * Returns the name of this custom tag.
+     * @return the name of this custom tag.
+     */
+    public String getName() {
+        return NAME;
+    }
+    
+    /**
+     * Will return true since <code>@assumption</code>
+     * can be used in field documentation.
+     * @return true since <code>@assumption</code>
+     * can be used in field documentation and false
+     * otherwise.
+     */
+    public boolean inField() {
+        return true;
+    }
+
+    /**
+     * Will return true since <code>@assumption</code>
+     * can be used in constructor documentation.
+     * @return true since <code>@assumption</code>
+     * can be used in constructor documentation and false
+     * otherwise.
+     */
+    public boolean inConstructor() {
+        return true;
+    }
+    
+    /**
+     * Will return true since <code>@assumption</code>
+     * can be used in method documentation.
+     * @return true since <code>@assumption</code>
+     * can be used in method documentation and false
+     * otherwise.
+     */
+    public boolean inMethod() {
+        return true;
+    }
+    
+    /**
+     * Will return true since <code>@assumption</code>
+     * can be used in method documentation.
+     * @return true since <code>@assumption</code>
+     * can be used in overview documentation and false
+     * otherwise.
+     */
+    public boolean inOverview() {
+        return true;
+    }
+
+    /**
+     * Will return true since <code>@assumption</code>
+     * can be used in package documentation.
+     * @return true since <code>@assumption</code>
+     * can be used in package documentation and false
+     * otherwise.
+     */
+    public boolean inPackage() {
+        return true;
+    }
+
+    /**
+     * Will return true since <code>@assumption</code>
+     * can be used in type documentation (classes or interfaces).
+     * @return true since <code>@assumption</code>
+     * can be used in type documentation and false
+     * otherwise.
+     */
+    public boolean inType() {
+        return true;
+    }
+    
+    /**
+     * Will return false since <code>@assumption</code>
+     * is not an inline tag.
+     * @return false since <code>@assumption</code>
+     * is not an inline tag.
+     */
+    
+    public boolean isInlineTag() {
+        return false;
+    }
+    
+    /**
+     * Register this Taglet.
+     * @param tagletMap  the map to register this tag to.
+     */
+    public static void register(Map tagletMap) {
+       AssumptionsTaglet tag = new AssumptionsTaglet();
+       Taglet t = (Taglet) tagletMap.get(tag.getName());
+       if (t != null) {
+           tagletMap.remove(tag.getName());
+       }
+       tagletMap.put(tag.getName(), tag);
+    }
+
+    /**
+     * Given the <code>Tag</code> representation of this custom
+     * tag, return its string representation.
+     * @param tag   the <code>Tag</code> representation of this custom tag.
+     */
+    public String toString(Tag tag) {
+        return "<DT><B>" + HEADER + "</B><DD>"
+               + tag.text()
+               + "\n";
+    }
+    
+    /**
+     * Given an array of <code>Tag</code>s representing this custom
+     * tag, return its string representation.
+     * @param tags  the array of <code>Tag</code>s representing of this custom tag.
+     */
+    public String toString(Tag[] tags) {
+        if (tags.length == 0) {
+            return null;
+        }
+        String result = "\n<DT><B>" + HEADER + "</B><DD>";
+        for (int i = 0; i < tags.length; i++) {
+            if (i > 0) {
+                result += "<DD>";
+            }
+            result += tags[i].text();
+        }
+        return result + "\n";
+    }
+}
---
+++ LicenseTaglet.java
@@ -0,0 +1,147 @@
+import com.sun.tools.doclets.Taglet;
+import com.sun.javadoc.*;
+import java.util.Map;
+
+/**
+ * A Taglet representing @license. This tag can be used in any kind of
+ * {@link com.sun.javadoc.Doc}.  It is not an inline tag. The text is displayed
+ * in like @param or @returns.
+ *
+ * @author Terin Stock
+ * @version 10.13.09
+ * @license GNU LGPL
+ */
+
+public class LicenseTaglet implements Taglet {
+    
+    private static final String NAME = "license";
+    private static final String HEADER = "License:";
+    
+    /**
+     * Returns the name of this custom tag.
+     * @return the name of this custom tag.
+     */
+    public String getName() {
+        return NAME;
+    }
+    
+    /**
+     * Will return true since <code>@license</code>
+     * can be used in field documentation.
+     * @return true since <code>@license</code>
+     * can be used in field documentation and false
+     * otherwise.
+     */
+    public boolean inField() {
+        return true;
+    }
+
+    /**
+     * Will return true since <code>@license</code>
+     * can be used in constructor documentation.
+     * @return true since <code>@license</code>
+     * can be used in constructor documentation and false
+     * otherwise.
+     */
+    public boolean inConstructor() {
+        return true;
+    }
+    
+    /**
+     * Will return true since <code>@license</code>
+     * can be used in method documentation.
+     * @return true since <code>@license</code>
+     * can be used in method documentation and false
+     * otherwise.
+     */
+    public boolean inMethod() {
+        return true;
+    }
+    
+    /**
+     * Will return true since <code>@license</code>
+     * can be used in method documentation.
+     * @return true since <code>@license</code>
+     * can be used in overview documentation and false
+     * otherwise.
+     */
+    public boolean inOverview() {
+        return true;
+    }
+
+    /**
+     * Will return true since <code>@license</code>
+     * can be used in package documentation.
+     * @return true since <code>@license</code>
+     * can be used in package documentation and false
+     * otherwise.
+     */
+    public boolean inPackage() {
+        return true;
+    }
+
+    /**
+     * Will return true since <code>@license</code>
+     * can be used in type documentation (classes or interfaces).
+     * @return true since <code>@license</code>
+     * can be used in type documentation and false
+     * otherwise.
+     */
+    public boolean inType() {
+        return true;
+    }
+    
+    /**
+     * Will return false since <code>@license</code>
+     * is not an inline tag.
+     * @return false since <code>@license</code>
+     * is not an inline tag.
+     */
+    
+    public boolean isInlineTag() {
+        return false;
+    }
+    
+    /**
+     * Register this Taglet.
+     * @param tagletMap  the map to register this tag to.
+     */
+    public static void register(Map tagletMap) {
+       LicenseTaglet tag = new LicenseTaglet();
+       Taglet t = (Taglet) tagletMap.get(tag.getName());
+       if (t != null) {
+           tagletMap.remove(tag.getName());
+       }
+       tagletMap.put(tag.getName(), tag);
+    }
+
+    /**
+     * Given the <code>Tag</code> representation of this custom
+     * tag, return its string representation.
+     * @param tag   the <code>Tag</code> representation of this custom tag.
+     */
+    public String toString(Tag tag) {
+        return "<DT><B>" + HEADER + "</B><DD>"
+               + tag.text()
+               + "\n";
+    }
+    
+    /**
+     * Given an array of <code>Tag</code>s representing this custom
+     * tag, return its string representation.
+     * @param tags  the array of <code>Tag</code>s representing of this custom tag.
+     */
+    public String toString(Tag[] tags) {
+        if (tags.length == 0) {
+            return null;
+        }
+        String result = "\n<DT><B>" + HEADER + "</B><DD>";
+        for (int i = 0; i < tags.length; i++) {
+            if (i > 0) {
+                result += "<DD>";
+            }
+            result += tags[i].text();
+        }
+        return result + "\n";
+    }
+}
